5f7242460d315f4ff03d3f3359af9583978b0710
Mindaugs Libietis <dpikse@gmail.com>
2021-02-25 16:53:22 +0200

    Homework2

---
+++ src/com/rcs/Homework2/Home2.java
@@ -0,0 +1,22 @@
+package com.rcs.Homework2;
+
+import java.util.Scanner;
+
+public class Home2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Lūdzu ievadi x koordināti : ");
+        int x = sc.nextInt();
+        System.out.println("Lūdzu ievadi y koordināti : ");
+        int y = sc.nextInt();
+        if (x > -2 && x < 3 && y > -4 && y < 1){
+            System.out.println("you are in target");
+        } else if (x >= -2 && x <= 3 && y >= -4 && y <= 1){
+            System.out.println("right on edge");
+        } else {
+            System.out.println("out of bounds");
+        }
+
+    }
+
+}
