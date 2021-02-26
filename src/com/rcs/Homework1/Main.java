fd1ebdaf5af0bb1abae604ae5a3027acd45e200c
Mindaugs Libietis <dpikse@gmail.com>
2021-02-26 11:42:24 +0200

    manor changes

--- src/com/rcs/Homework1/Main.java
+++ src/com/rcs/Homework1/Main.java
@@ -10,6 +10,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter temperature in Farenheits:");
         x = sc.nextDouble();
+        sc.close();
         y = (5 * (x - 32)) / 9;
         double rounded = Math.round(y * 100) / 100.0;
         System.out.println("Temperature in celsius = " + y);
@@ -17,4 +18,4 @@ public class Main {
     }
 
 }
-}
+
