b7f6b439c1cf54f95f17b69d314d9950b0678fe3
Mindaugs Libietis <dpikse@gmail.com>
2021-02-26 11:36:47 +0200

    manor changes

--- src/com/rcs/Homework1/Main.java
+++ src/com/rcs/Homework1/Main.java
@@ -11,22 +11,23 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter temperature in Farenheits:");
         x = sc.nextDouble();
-        y = (5 * ( x - 32 ) ) / 9;
-        double rounded = Math.round (y * 100)/100.0;
+        y = (5 * (x - 32)) / 9;
+        double rounded = Math.round(y * 100) / 100.0;
         System.out.println("Temperature in celsius = " + y);
         System.out.println("Temperature in celsius rounded = " + rounded);
-        //System.out.printf("Temperature in celsius rounded = %.2f" + "\n", y);
         System.out.println("Would you wish to convert c to f? y or n :");
         z = sc.next().charAt(0);
         if (z == 'y') {
             System.out.println("Enter temperature in celsius: ");
             c = sc.nextDouble();
             sc.close();
-            v = ( c * 9 / 5 + 32);
+            v = (c * 9 / 5 + 32);
             System.out.println("Temperature in farenheit = " + v);
             System.out.printf("Temperature in farenheit rounded = %.2f" + "\n", v);
-        } else if (z == 'n'){
+        } else if (z == 'n') {
             System.out.println("End of this programm");
+        } else {
+            System.out.println("bad operator");
         }
 
     }
