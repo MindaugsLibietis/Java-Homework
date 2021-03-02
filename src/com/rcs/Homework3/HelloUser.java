96496a7061b165317f9d11b882913a372abb1ea4
Mindaugs Libietis <dpikse@gmail.com>
2021-03-02 18:57:09 +0200

    versija v2

--- src/com/rcs/Homework3/HelloUser.java
+++ src/com/rcs/Homework3/HelloUser.java
@@ -1,5 +1,6 @@
 package com.rcs.Homework3;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class HelloUser {
@@ -7,12 +8,24 @@ public class HelloUser {
         Scanner sc = new Scanner (System.in);
         System.out.println("Lūdzu ievadiet vārdu un uzvārdu : ");
         String fullName = sc.nextLine();
-        String[] splitString = fullName.split(" ");
+        sc.close();
+        /*versija 1 strādā ar vairākiem uzvārdiem, bet nestrādā ar vairākām atstarpēm*/
+/*        String[] splitString = fullName.split(" ");
         String firstLetter = splitString[0].substring(0 ,1);
         String remainingLetters = splitString[0].substring(1, splitString[0].length());
         firstLetter = firstLetter.toUpperCase();
         splitString[0] = firstLetter + remainingLetters;
-        System.out.println("'" + splitString[0] + "'");
-        System.out.println("'" + splitString[1].toUpperCase() + "'");
+        System.out.println("jusu ievaditais vards : " + fullName);
+        System.out.println("jusu vards : " + "'" + splitString[0] + "'");
+        System.out.println("jusu uzvards : " + "'" + splitString[1].toUpperCase() + "'");
+        System.out.println("jusu otrais uzvards : " + "'" + splitString[2].toUpperCase() + "'");*/
+
+        /*versija 2 strādā ar vairākām atstarpēm, bet nestrādā ar vairākiem uzvārdiem*/
+        fullName = fullName.trim();
+        String name = fullName.substring(0, fullName.indexOf(" "));
+        String surname = fullName.substring(fullName.indexOf(" "));
+        System.out.println("ievadītais vārds : " + fullName);
+        System.out.println("jusu vards : " + "'" + name.substring(0, 1).toUpperCase() + name.substring(1) + "'");
+        System.out.println("jusu uzvards : " + "'" + surname.trim().toUpperCase() + "'");
     }
 }
