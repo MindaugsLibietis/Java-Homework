8cec685169c1195784b4e347d0fbeed1d3774895
Mindaugs Libietis <dpikse@gmail.com>
2021-03-13 20:20:02 +0200

    manor changes

--- src/com/rcs/Homework5/Matrices.java
+++ src/com/rcs/Homework5/Matrices.java
@@ -7,28 +7,29 @@ public class Matrices {
     public static void main(String[] args) {
         Random rnd = new Random();
         Scanner sc = new Scanner(System.in);
-        int n = rnd.nextInt(4)+3;
-        int[][] array = new int [n][n];
+        int n = rnd.nextInt(4) + 3;
+        int[][] array = new int[n][n];
         System.out.println("Do you wish to fill array manualy? (yes - y ; no - n):");
         char mor = sc.next().charAt(0);
-        if (mor == 'y'){
-            for(int i = 0; i< array.length; i++) {
+        if (mor == 'y') {
+            for (int i = 0; i < array.length; i++) {
                 for (int j = 0; j < array[i].length; j++) {
                     System.out.println("Matrix" + "[" + i + "]" + "[" + j + "]");
                     array[i][j] = sc.nextInt();
                 }
                 System.out.println();
             }
-            for(int i = 0; i< array.length; i++){
-                for(int j = 0 ;j< array[i].length; j++){
+            sc.close();
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
                     System.out.printf("%5d", array[i][j]);
                 }
                 System.out.println();
             }
             System.out.println();
-            for(int i = 0; i< array.length; i++){
-                for(int j = 0 ;j< array[i].length; j++){
-                    if(i == j) {
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    if (i == j) {
                         System.out.printf("%5d", array[i][j]);
                     } else {
                         System.out.printf("%5d", 0);
@@ -36,18 +37,18 @@ public class Matrices {
                 }
                 System.out.println();
             }
-        } else if (mor == 'n'){
+        } else if (mor == 'n') {
             for (int i = 0; i < array.length; i++) {
                 for (int j = 0; j < array[i].length; j++) {
-                    array[i][j]= rnd.nextInt(101);
+                    array[i][j] = rnd.nextInt(100) + 1;
                     System.out.printf("%5d", array[i][j]);
-                    }
-                System.out.println();
                 }
+                System.out.println();
+            }
             System.out.println();
             for (int i = 0; i < array.length; i++) {
                 for (int j = 0; j < array[i].length; j++) {
-                    if(i == j) {
+                    if (i == j) {
                         System.out.printf("%5d", array[i][j]);
                     } else {
                         System.out.printf("%5d", 0);
