bcd2da33d3c132007d6719f1e05084cc7d94eba7
Mindaugs Libietis <dpikse@gmail.com>
2021-03-21 21:47:45 +0200

    koda uzkopšana

--- src/com/rcs/Homework4/SortArray.java
+++ src/com/rcs/Homework4/SortArray.java
@@ -18,25 +18,22 @@ public class SortArray {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = ran.nextInt(90) + 10;
         }
-//        System.out.println("Random array : " + Arrays.toString(arr));
         System.out.print("Random array : ");
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + ((i < arr.length - 1) ? " , " : ""));
+            System.out.print(arr[i] + ((i < arr.length - 1) ? ", " : "\n"));
         }
         for (int i = 0; i < arr.length; i++) {
             for (int j = 1; j < (arr.length - i); j++) {
-                int temp = 0;
                 if (arr[j - 1] > arr[j]) {
-                    temp = arr[j - 1];
+                    int temp = arr[j - 1];
                     arr[j - 1] = arr[j];
                     arr[j] = temp;
                 }
             }
         }
-        System.out.println();
         System.out.print("Sorted array : ");
         for (int i = 0; i < arr.length; i++){
-            System.out.print(arr[i] + ((i< arr.length - 1) ? " , " : ""));
+            System.out.print(arr[i] + ((i< arr.length - 1) ? ", " : ""));
         }
 
     }
