dffffc55d2648ac6acd4de65e00186dbe4a4e746
Mindaugs Libietis <dpikse@gmail.com>
2021-03-21 22:02:17 +0200

    koda uzkopšana

--- src/com/rcs/Homework5/Matrices.java
+++ src/com/rcs/Homework5/Matrices.java
@@ -11,54 +11,45 @@ public class Matrices {
         int[][] array = new int[n][n];
         System.out.println("Do you wish to fill array manualy? (yes - y ; no - n):");
         char mor = sc.next().charAt(0);
-        if (mor == 'y') {
-            for (int i = 0; i < array.length; i++) {
-                for (int j = 0; j < array[i].length; j++) {
-                    System.out.printf("Matrix [%d] [%d]", i , j);
-                    array[i][j] = sc.nextInt();
+        switch(Character.toUpperCase(mor)) {
+            case 'Y':
+                for (int i = 0; i < array.length; i++) {
+                    for (int j = 0; j < array[i].length; j++) {
+                        System.out.printf("Matrix [%d] [%d]", i, j);
+                        array[i][j] = sc.nextInt();
+                    }
+                    System.out.println();
                 }
-                System.out.println();
-            }
-            sc.close();
-            for(int[] row : array){
-                for (int column : row){
-                    System.out.printf("%5d", column);
+                break;
+            case 'N':
+                for (int i = 0; i < array.length; i++) {
+                    for (int j = 0; j < array[i].length; j++) {
+                        array[i][j] = rnd.nextInt(100) + 1;
+                    }
+                    System.out.println();
                 }
-                System.out.println();
+                break;
+            default:
+                System.out.println("Wrong input");
+                return;
+        }
+        sc.close();
+        for (int[] row : array){
+            for (int column : row){
+                System.out.printf("%5d", column);
             }
             System.out.println();
-            for (int i = 0; i < array.length; i++) {
-                for (int j = 0; j < array[i].length; j++) {
-                    if (i == j) {
-                        System.out.printf("%5d", array[i][j]);
-                    } else {
-                        System.out.printf("%5d", 0);
-                    }
-                }
-                System.out.println();
-            }
-        } else if (mor == 'n') {
-            for (int i = 0; i < array.length; i++) {
-                for (int j = 0; j < array[i].length; j++) {
-                    array[i][j] = rnd.nextInt(100) + 1;
+        }
+        System.out.println();
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[i].length; j++){
+                if (i == j){
                     System.out.printf("%5d", array[i][j]);
+                }else{
+                    System.out.printf("%5d",0);
                 }
-                System.out.println();
             }
             System.out.println();
-            for (int i = 0; i < array.length; i++) {
-                for (int j = 0; j < array[i].length; j++) {
-                    if (i == j) {
-                        System.out.printf("%5d", array[i][j]);
-                    } else {
-                        System.out.printf("%5d", 0);
-                    }
-                }
-                System.out.println();
-            }
-
-        } else {
-            System.out.println("Wrong input");
         }
     }
 }
